61c10d0d4621e2a5863590661aec9c63b32414af
MostafaMohamed86 <mostafa.mohamed0869@gmail.com>
2025-03-20 19:21:58 +0200

    Fix server port issue

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 
